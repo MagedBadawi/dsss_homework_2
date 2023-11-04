c2472ef67bff9a2a2ea9760ff42b8edd551f6d66
MagedBadawi <117099847+MagedBadawi@users.noreply.github.com>
2023-11-04 03:39:19 +0200

    Update tests_math_quiz.py
    unittests added

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,41 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_number_fun, random_number_fun, random_math_process
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_random_number_fun(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = random_number_fun(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_number_fun(self):
+        # Test if random mathematical symbolegenerated  is correct
+        for _ in range(1000):  # Test a large number of random values
+            rand_symbole = random_number_fun()
+            self.assertTrue(rand_symbole == "+" or rand_symbole == "-" or  rand_symbole == "*" )
+    
 
-    def test_function_C(self):
+    def test_random_math_process(self):
             test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '+', '5 + 2', 7), (6, 4, '-','6-1',5), (2, 3, '*', '2*3', 6), (60, 90, '+', '60+90', 150)
+                
             ]
 
             for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+                if operator == "+": 
+                     self.assertTrue(expected_answer = num1 + num2)
+                elif operator == '-': 
+                     self.assertTrue(expected_answer = num1 - num2)
+                else operator == '*': 
+                     self.assertTrue(expected_answer = num1 * num2)
+
+                    
+    
 
 if __name__ == "__main__":
     unittest.main()
+    
